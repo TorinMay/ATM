0592979de3530c462c780d615e1388186100894c
TorinMay <mayte@s.dcsdk12.org>
2013-10-10 09:24:14 -0600

    Worked on login and users

--- ATM.java
+++ ATM.java
@@ -1,39 +1,45 @@
 import java.util.Scanner;
 
 public class ATM {
-	//MAIN
-	private User active;
-	private User testAccount1 = user(Bob, 1234, 100.00);
-	private User testAccount2 = user(Alice, 3333, 250.00);
-	private User testAccount3 = user(JT, 0001, 999999999.00);
+	//State
+	private String name;
+	private int pin;
+	private double balance;
 
+	public ATM() {
+
+	}
+
+	
 	public void user(String name, int pin, double balance) {
-		
+		String[] names = new Array{"Bob", "Alice", "JT"};
+		int[] pins = new Array{1234, 3333, 0001};
+		double[] balances = new Array{100.00, 250.00, 999999999.00};
 	}
 	public void checkBalance(double balance) {
 		
 	}
 
-	public static String login() {
+	public String login() {
 		System.out.print("Please input account name: ");
 		Scanner login = new Scanner(System.in);
-		String name = login;
+		String name = login.nextLine();
 		return name;
 	}
 
 	public void logout() {
 
 	}
-
-	public String changeName() {
-
-	}
+	// public String name() {
+	// 	return name;
+	// }
+	// public String changeName() {
+	// 	if (name.length() > 2 && name.length() < 20) {
+	// 		this.name = name;
+	// 	}
+	// }
 	
-	public int withDraw() {
+	// public int withDraw() {
 
-	}
-
-	public static void main(String[] args) {
-		login();
-	}
+	// }
 }
