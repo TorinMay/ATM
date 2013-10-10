f80a2b0383571bf2b9b7f79bd744239046646a20
TorinMay <mayte@s.dcsdk12.org>
2013-10-10 08:37:50 -0600

    Added all methods, started on methods

--- ATM.java
+++ ATM.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
+
 public class ATM {
+	//MAIN
+	public void user() {
+		
+	}
+	public void checkBalance() {
+
+	}
+
+	public void login() {
+		System.out.print("Please input account name: ");
+		Scanner login = new Scanner(System.in);
+	}
+
+	public void logout() {
+
+	}
+
+	public String changeName() {
+
+	}
 	
+	public int withDraw() {
+
+	}
+
+	public static void main(String[] args) {
+		login();
+	}
 }
