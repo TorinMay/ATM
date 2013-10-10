a014d762a39303b58c1f290713b0c046d0e7574a
TorinMay <mayte@s.dcsdk12.org>
2013-10-10 08:54:06 -0600

    Main method is made

--- ATM.java
+++ ATM.java
@@ -2,16 +2,23 @@ import java.util.Scanner;
 
 public class ATM {
 	//MAIN
-	public void user() {
+	private User active;
+	private User testAccount1 = user(Bob, 1234, 100.00);
+	private User testAccount2 = user(Alice, 3333, 250.00);
+	private User testAccount3 = user(JT, 0001, 999999999.00);
+
+	public void user(String name, int pin, double balance) {
 		
 	}
-	public void checkBalance() {
-
+	public void checkBalance(double balance) {
+		
 	}
 
-	public void login() {
+	public static String login() {
 		System.out.print("Please input account name: ");
 		Scanner login = new Scanner(System.in);
+		String name = login;
+		return name;
 	}
 
 	public void logout() {
---
+++ ATMRunner.java
@@ -0,0 +1,6 @@
+class ATMRunner {
+
+	public static void main(String[] args) {
+		
+	}
+}
